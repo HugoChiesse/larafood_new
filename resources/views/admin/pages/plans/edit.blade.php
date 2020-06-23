148f5d6b57e435963a72921046bb26d1df35cb8f
Hugo Ferreira Chiesse <hugochiesse@gmail.com>
2020-06-23 11:56:41 -0300

    Create/Edit/Show model PLAN

--- resources/views/admin/pages/plans/edit.blade.php
+++ resources/views/admin/pages/plans/edit.blade.php
@@ -3,6 +3,13 @@
 @section('title', $title)
 
 @section('content_header')
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a></li>
+        <li class="breadcrumb-item"><a href="{{ route('plans.index') }}">Planos</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Visualizar</li>
+    </ol>
+</nav>
 <h1>{{ $title }}</h1>
 @stop
 
